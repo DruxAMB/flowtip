5be89e7705bf2ec058aa2a043df902f73581b2c0
DruxAMB <druxambmarketing@gmail.com>
2025-02-23 22:14:21 +0100

    deployed a new contract & added alert ui component

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,1 +1,2 @@
-export const TipflowFactoryAddress = "0x6f0cE7E5E1E0EE0A0E0E0E0E0E0E0E0E0E0E0E0E" as const;
+// Replace this with your deployed contract address from hardhat deploy
+export const TipflowFactoryAddress = "0x63388A88fD3e89e6C978f7D7c521698A0ee3b2dB" as const;
