37d3f3b9e5f9aebcccf9a28f47e3ca73d61e8a1a
DruxAMB <druxambmarketing@gmail.com>
2025-02-28 11:41:28 +0100

    fixed the imports and function errors in the tip/page.ts dir

--- frontend/src/app/(tip)/tip/[username]/page.tsx
+++ frontend/src/app/(tip)/tip/[username]/page.tsx
@@ -1,10 +1,10 @@
 import { readContract } from "@wagmi/core";
 import TipForm from "./tip-form";
 import Image from "next/image";
-import { CryptoStreamrFactoryAddress } from "@/constants";
-import { CryptoStreamrFactoryAbi } from "@/abi/CryptoStreamrFactory";
+import { TipflowFactoryAddress } from "@/constants";
+import { TipflowFactoryAbi } from "@/abi/TipflowFactory";
 import { config } from "@/wagmi";
-import { CryptoStreamrAbi } from "@/abi/CryptoStreamr";
+import { TipflowAbi } from "@/abi/Tipflow";
 
 export default async function Page({
   params,
@@ -14,8 +14,8 @@ export default async function Page({
   const { username } = await params;
 
   const result = await readContract(config, {
-    abi: CryptoStreamrFactoryAbi,
-    address: CryptoStreamrFactoryAddress,
+    abi: TipflowFactoryAbi,
+    address: TipflowFactoryAddress,
     functionName: "creatorInfoByUsername",
     args: [username],
   });
@@ -23,12 +23,6 @@ export default async function Page({
   if (result[0] === "") {
     return <div>Creator not found</div>;
   }
-
-  const bioResult = await readContract(config, {
-    abi: CryptoStreamrAbi,
-    address: result[2],
-    functionName: "bio",
-  });
 
   return (
     <div className="flex flex-col items-center justify-center bg-white rounded-xl p-4 gap-2 max-w-md mx-4 shadow-md border min-w-[450px]">
@@ -40,7 +34,7 @@ export default async function Page({
         className="size-16 rounded-full"
       />
       <div>Tip to {username}</div>
-      <div className="text-sm text-gray-500 mb-1">{bioResult}</div>
+      <div className="text-sm text-gray-500 mb-1">{result[1]}</div>
       <TipForm creatorAddress={result[1]} contractAddress={result[2]} />
     </div>
   );
